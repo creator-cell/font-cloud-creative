7bb7f8fb385d41367c48491076965070c1a606a0
AnkitGupta <ankitgupta72950@gmail.com>
2025-12-05 17:49:36 +0530

    Padding Decrease in footer

--- apps/web/components/test-center/section/FooterSection.tsx
+++ apps/web/components/test-center/section/FooterSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const FooterSection = () => {
   return (
-    <footer className="w-full py-10 flex items-center justify-center bg-white dark:bg-slate-900 mt-10 ">
+    <footer className="w-full 2xl:py-10 py-4 flex items-center justify-center bg-white dark:bg-slate-900 mt-10 ">
       <div className="text-center flex items-center justify-center gap-4">
         <p className="text-xs tracking-[3px] font-semibold text-[#3d2b63] mb-1 dark:text-[#f2f6fa] ">
           POWERED BY:
